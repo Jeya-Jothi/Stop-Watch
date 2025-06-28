09d3355f187c4baa13a4cebba39b3d119ff35a79
Jeya Jothi <jeyajothi108@gmail.com>
2025-06-28 12:57:11 +0530

    Update contact

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -3,11 +3,22 @@ function About() {
   return (
     <div className="about-container">
       <div className="about-section hidden" id="about">
-        <p className="about-title">About This Stopwatch Timer</p>
+        <p className="about-title">About</p>
         <p>
-          This is one of 20 creative projects built using React and CSS. It
-          features glassmorphism, animations, and state management.
+          This is a simple stopwatch application built using React. It allows
+          users to start, stop, pause, reset, and record lap times with smooth
+          updates and precise timing logic. The timer logic is powered by{" "}
+          <code>useRef</code> to manage internal state efficiently without
+          unnecessary re-renders, and the UI is updated using{" "}
+          <code>useState</code> for accurate and reactive display.
         </p>
+
+        <p className="key-features">Key features</p>
+        <div className="key-card">
+          <p>Live updating timer in minutes, seconds, and milliseconds</p>
+          <p>Start, Stop, Pause, and Reset controls</p>
+          <p>Lap time recording with dynamic display</p>
+        </div>
       </div>
     </div>
   );
--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -2,9 +2,67 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact-container">
-      <div className="contact">
-        <p className="contact-title">Contact info</p>
-        <p>cdivieyfeuibcjdc</p>
+      <div className="contact-card">
+        <p className="contact-title">Contact</p>
+
+        <div className="reach-me-card">
+          <div>
+            <a
+              href="mailto:jeyajothi108@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="images/email logo.svg"
+                alt="email"
+                className="contact-logo"
+              />
+              jeyajothi108@gmail.com
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/jeya-jothi-841277211"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="images/linkedin logo.svg"
+                alt="linkedin"
+                className="contact-logo"
+              />
+              Linkedin
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/Jeya-Jothi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="images/github logo.svg"
+                alt="github"
+                className="contact-logo"
+              />
+              GitHub
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://jothi-portfolio-9utu.onrender.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="images/logo jj.svg"
+                alt="portfolio"
+                className="contact-logo"
+              />
+              Portfolio
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
